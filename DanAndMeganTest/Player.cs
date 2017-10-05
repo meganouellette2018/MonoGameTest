915f48c18605987b2863cd9cf47d032ee4eb5d25
Megan Ouellette <m.ouellette@baypath.net>
2017-10-05 11:20:22 -0400

    Updated Player Class

--- DanAndMeganTest/Player.cs
+++ DanAndMeganTest/Player.cs
@@ -11,6 +11,34 @@ namespace DanAndMeganTest
 {
     public class Player : DrawableGameComponent
     {
+
+        private Texture2D solidTexture;
+        private Rectangle boundingBox;
+        private int xVelocity, yVelocity;
+        private SpriteBatch sb;
+
+
+        public Player (Game game, SpriteBatch sb, int x, int y, int xv, int yv) : base(game)
+        {
+            this.sb = sb;
+            solidTexture = new Texture2D(GraphicsDevice, 1, 1);
+            solidTexture.SetData(new Color[] { Color.White });
+            boundingBox = new Rectangle(x, y, 75, 75);
+            xVelocity = xv;
+            yVelocity = yv;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+
+
+
+
+            base.Update(gameTime);
+
+
+        }
     }
 }
 
