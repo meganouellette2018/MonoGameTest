3e4af561942fb35cf2a7eab1226d7f877d17faeb
Megan Ouellette <m.ouellette@baypath.net>
2017-10-06 09:09:53 -0400

    not sure

--- DanAndMeganTest/Enemies.cs
+++ DanAndMeganTest/Enemies.cs
@@ -27,12 +27,12 @@ namespace DanAndMeganTest
         public override void Update(GameTime gameTime)
         {
 
-            if (boundingBox.X <= 0 || boundingBox.X >= 1150)
+            if (boundingBox.X <= 0 || boundingBox.X >= 1125)
                 xVelocity *= -1;
 
             boundingBox.X += xVelocity;
 
-            if (boundingBox.Y <= 0 || boundingBox.Y >= 550)
+            if (boundingBox.Y <= 0 || boundingBox.Y >= 525)
                 yVelocity *= -1;
 
             boundingBox.Y += yVelocity;
